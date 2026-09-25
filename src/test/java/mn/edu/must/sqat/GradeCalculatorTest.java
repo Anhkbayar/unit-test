c971bebea035787a01977913f74de59c161ca8d8
З.Анхбаяр <B232270029@must.edu.mn>
2026-09-25 16:40:43 +0800

    parameterized test

--- src/test/java/mn/edu/must/sqat/GradeCalculatorTest.java
+++ src/test/java/mn/edu/must/sqat/GradeCalculatorTest.java
@@ -2,6 +2,9 @@ package mn.edu.must.sqat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GradeCalculatorTest {
@@ -10,8 +13,18 @@ public class GradeCalculatorTest {
     @Test
     @DisplayName("90 оноо яг A дүн байх ёстой")
     void ninetyIsExactlyA(){
-        GradeCalculator calc = new GradeCalculator();
-        String grade = calc.letterGrade(90.0);
+        String grade = calculator.letterGrade(90.0);
         assertEquals("A", grade);
     }
+
+    @ParameterizedTest
+    @CsvFileSource(
+            resources = "/grades.csv",
+            numLinesToSkip = 1
+    )
+    @DisplayName("Оноо зөв үсгэн дүн рүү хөрвөх ёстой")
+    void gradeTest(double score, String expected){
+        assertEquals(expected, calculator.letterGrade(score));
+    }
+
 }

54682afc6345f5b056e38cdc1bc252ca00d282e6
З.Анхбаяр <B232270029@must.edu.mn>
2026-09-25 17:58:05 +0800

    parameterized test and result

--- src/test/java/mn/edu/must/sqat/GradeCalculatorTest.java
+++ src/test/java/mn/edu/must/sqat/GradeCalculatorTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class GradeCalculatorTest {
     private final GradeCalculator calculator = new GradeCalculator();
@@ -17,6 +18,37 @@ public class GradeCalculatorTest {
         assertEquals("A", grade);
     }
 
+    @Test
+    @DisplayName("Дүн хасах утгатай байхад алдаа өгөх")
+    void negativeScore(){
+        assertThrows(IllegalArgumentException.class,
+                ()-> calculator.letterGrade(-15.0));
+    }
+
+    @Test
+    @DisplayName("Дүн хязгааргй утгатай байхад алдаа өгөх")
+    void infiniteScore(){
+        assertThrows(IllegalArgumentException.class,
+                ()-> calculator.letterGrade(Double.POSITIVE_INFINITY)
+        );
+    }
+
+    @Test
+    @DisplayName("Дүн 100-аас их байхад алдаа өгөх")
+    void aboveHundredScore(){
+        assertThrows(IllegalArgumentException.class,
+                ()-> calculator.letterGrade(102.52)
+        );
+    }
+
+    @Test
+    @DisplayName("Нийт дүн бодох")
+    void totalScore(){
+        Grade grade = new Grade(8,35, 10,9, 25);
+        Double total = calculator.totalScore(grade);
+        assertEquals(87, total);
+    }
+
     @ParameterizedTest
     @CsvFileSource(
             resources = "/grades.csv",
@@ -25,6 +57,25 @@ public class GradeCalculatorTest {
     @DisplayName("Оноо зөв үсгэн дүн рүү хөрвөх ёстой")
     void gradeTest(double score, String expected){
         assertEquals(expected, calculator.letterGrade(score));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(
+        resources = "/total_score.csv",
+            numLinesToSkip = 1
+    )
+    @DisplayName("Нийт дүн зөв бодогдох ёстой")
+    void totalScore(
+            double att,
+            double lab,
+            double quiz1,
+            double quiz2,
+            double exam,
+            double expectedTotal
+    ){
+        Grade grade = new Grade(att, lab, quiz1, quiz2, exam);
+        double total = calculator.totalScore(grade);
+        assertEquals(expectedTotal, total);
     }
 
 }
